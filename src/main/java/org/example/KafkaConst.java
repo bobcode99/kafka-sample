4d34294b2c702586986d193f5b0d250c003dfabb
yb <lojobs38@gmail.com>
2025-03-24 00:19:07 +0800

    Update const

--- src/main/java/org/example/KafkaConst.java
+++ src/main/java/org/example/KafkaConst.java
@@ -1,6 +1,13 @@
 package org.example;
 
 public class KafkaConst {
-    public static final String TOPIC = "test-topic";
-    public static final String GROUP = "test-group";
+    public static final String TOPIC = "test-topic-2";
+
+    public static final String GROUP = "test-group-2";
+
+    public static final String TOPIC_TRANSACTIONAL = "test-topic-transac-1";
+    public static final String GROUP_TRANSACTIONAL = "test-group-transac-1";
+
+
+    public static final String SIMPLE_TOPIC_FOR_EOS = "simple-topic";
 }
